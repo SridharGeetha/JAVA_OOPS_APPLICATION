e153f3094f3ad1284f5d4dec97e08fc880bd4596
SridharGeetha <ssridhar4858@gmail.com>
2025-08-03 00:09:57 +0530

    Add clean code

--- Parking_Mangement_System/Parking_System.java
+++ Parking_Mangement_System/Parking_System.java
@@ -4,21 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class VehicleInfo{
+class Vehicle{
 
     private int slotnumber;
     private String vehicleNumber;
     private String vehicleType; 
     private String vehicleColor;
     
-    public VehicleInfo(int slotNumber,String vehicleNumber, String vehicleType, String vehicleColor) {
+    public Vehicle(int slotNumber,String vehicleNumber, String vehicleType, String vehicleColor) {
         this.slotnumber = slotNumber;
         this.vehicleNumber = vehicleNumber;
         this.vehicleType = vehicleType;
         this.vehicleColor = vehicleColor;
     }
 
-    public VehicleInfo() {
+    public Vehicle() {
         
     }
     public int getSlotnumber() {
@@ -53,26 +53,112 @@ class VehicleInfo{
         this.vehicleColor = vehicleColor;
     }
 
-    public void RemoveVehicleBySlotNumber(int slotNumber,List<VehicleInfo> vehicleInfos){
-        for(VehicleInfo vInfo : vehicleInfos){
-            if(vInfo.getSlotnumber() == slotNumber){
-                vehicleInfos.remove(vInfo);
+}
+
+class ParkingLot{
+    private int numberofSlots;
+    private boolean[] slots;
+    private List<Vehicle> vehicleDetails;
+
+    public ParkingLot(int numberofSlots) {
+        this.numberofSlots = numberofSlots;
+        this.slots = new boolean[numberofSlots+1];
+        this.vehicleDetails = new ArrayList<>();
+    }
+    public int getNumberofSlots() {
+        return numberofSlots;
+    }
+    public void setNumberofSlots(int numberofSlots) {
+        this.numberofSlots = numberofSlots;
+    }
+    public boolean[] getSlots() {
+        return slots;
+    }
+    public void setSlots(boolean[] slots) {
+        this.slots = slots;
+    }
+    public List<Vehicle> getVehicleDetails() {
+        return vehicleDetails;
+    }
+    public void setVehicleDetails( Vehicle vehicle) {
+        this.vehicleDetails.add(vehicle);
+    }
+
+    void parkVehicle(Scanner sc){
+
+        for(int i=1;i<=numberofSlots;i++){
+            System.out.println("Available Slots.");
+            if(!slots[i]){
+                System.out.println("Slots : "+i);
+            }
+        }
+        System.out.println();
+
+        System.out.println("Enter Slot to Park Vehicle");
+        int slotNumber = sc.nextInt();
+        sc.nextLine();
+        if(slotNumber<=0 || slotNumber>numberofSlots+1){
+              System.out.println("Invalid Slot number");
+              return;
+          }
+          if(!slots[slotNumber]){
+              System.out.println("Welcome!");
+              System.out.println("Enter Vehicle Number");
+              String vehicleNumber = sc.nextLine();
+              System.out.println("Enter Vehicle Type");
+              String vehicleType = sc.nextLine();
+              System.out.println("Enter Vehicle Color");
+              String vehicleColor = sc.nextLine();
+              Vehicle VehicleObject = new Vehicle(slotNumber,vehicleNumber,vehicleType,vehicleColor);
+              System.out.println(VehicleObject.getVehicleNumber());
+              System.out.println(VehicleObject);
+              vehicleDetails.add(VehicleObject);
+              slots[slotNumber]=true;
+              System.out.println("Vehicle Added Successfully!");
+          }else{
+              System.out.println("Slot is not Available");
+          }
+    }
+
+    void RemoveVehicle(Scanner sc){
+        System.out.println("Enter Slot Number to Remove vehicle");
+        int slotNumberToRemove = sc.nextInt();
+        for(Vehicle vInfo : vehicleDetails){
+            if(vInfo.getSlotnumber() == slotNumberToRemove){
+                vehicleDetails.remove(vInfo);
+                slots[slotNumberToRemove] = false;
+                System.out.println("Vehicle removed Successfully");
                 return;
             }
         }
         System.out.println("Vehicle cannot found!");
     }
 
+    void viewSlot(Scanner sc){
+         System.out.println("Parking Slot Details");
+         for (int slotIndex = 1; slotIndex <= numberofSlots; slotIndex++) {
+             if(!slots[slotIndex]){
+                 System.out.println("Parking Slot "+slotIndex+" Available");
+             }else{
+                 System.out.println("Parking Slot "+slotIndex+" Full");
+             }
+         }
+    }
+
+    public void viewVehicle(Scanner sc) {
+        System.out.println("Type          Color       Slot");
+        for (Vehicle vehicle : vehicleDetails) {
+            System.out.println();
+            System.out.println(vehicle.getVehicleType()+"           "+vehicle.getVehicleColor()+"          "+vehicle.getSlotnumber());
+        }    
+    }
 }
 
 public class Parking_System {
     public static void main(String[] args) throws InterruptedException {
         Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter Number of Slots");
         int numberofSlots = 10;
-        boolean[] slots = new boolean[numberofSlots+1];
-        VehicleInfo vehicleObject = new VehicleInfo();
-        List<VehicleInfo> vehicleInfos = new ArrayList<>();
+        ParkingLot parking = new ParkingLot(numberofSlots);
         boolean isvalid = true;
         for (int i = 0; i < 5; i++) {
             Thread.sleep(100);
@@ -88,61 +174,31 @@ public class Parking_System {
             System.out.println();
             System.out.println("1.Park Vehicle");
             System.out.println("2.Remove Vehicle");
-            System.out.println("3.View Vehicle");
-            System.out.println("4.exit");
+            System.out.println("3.View Slot");
+            System.out.println("4.View Vehicle");
+            System.out.println("5.exit");
             int choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    System.out.println("Enter Slot to Park Vehicle");
-                    int slotNumber = sc.nextInt();
-                    sc.nextLine();
-                    if(slotNumber<=0 || slotNumber>numberofSlots){
-                        System.out.println("Invalid Slot number");
-                        break;
-                    }
-                    if(!slots[slotNumber]){
-                        System.out.println("Welcome!");
-                        System.out.println("Enter Vehicle Number");
-                        String vehicleNumber = sc.nextLine();
-                        System.out.println("Enter Vehicle Type");
-                        String vehicleType = sc.nextLine();
-                        System.out.println("Enter Vehicle Color");
-                        String vehicleColor = sc.nextLine();
-                        VehicleInfo vehicleInfoObject = new VehicleInfo(slotNumber,vehicleNumber,vehicleType,vehicleColor);
-                        vehicleInfos.add(vehicleInfoObject);
-                        slots[slotNumber]=true;
-                        System.out.println("Vehicle Added Successfully!");
-                    }else{
-                        System.out.println("Slot is not Available");
-                    }
+                    parking.parkVehicle(sc);
                     break;
                 case 2:
-                    System.out.println("Enter Slot Number to Remove vehicle");
-                    int slotNumberToRemove = sc.nextInt();
-                    if(slots[slotNumberToRemove]){
-                        vehicleObject.RemoveVehicleBySlotNumber(slotNumberToRemove, vehicleInfos);
-                        slots[slotNumberToRemove] = false;
-                    }else{
-                        System.out.println("Slot Empty No vehicle to remove");
-                    }
+                    parking.RemoveVehicle(sc);
                     break;
                 case 3:
-                    System.out.println("Parking Slot Details");
-                    for (int slotIndex = 1; slotIndex <= numberofSlots; slotIndex++) {
-                        if(!slots[slotIndex]){
-                            System.out.println("Parking Slot "+slotIndex+" Available");
-                        }else{
-                            System.out.println("Parking Slot "+slotIndex+" Full");
-                        }
-                    }
+                   parking.viewSlot(sc);
                     break;
                 case 4:
+                    parking.viewVehicle(sc);
+                    break;
+                case 5:
                     System.out.println("<<<<<<<Exit>>>>>>");
                     isvalid = false;
                     Thread.sleep(3000);
-                    break;
+                break;
+
                 default:
-                    System.out.println("nothing");
+                    System.out.println("Invalid option");
                     break;
             }
         }
